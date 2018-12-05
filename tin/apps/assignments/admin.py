e4d63d766c6ce86fe3b4f1c5c23e4b85969ae1e2
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-04 23:36:58 -0500

    Add things to admin interface

--- tin/apps/assignments/admin.py
+++ tin/apps/assignments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Assignment
 
 # Register your models here.
+
+class AssignmentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Assignment, AssignmentAdmin)
