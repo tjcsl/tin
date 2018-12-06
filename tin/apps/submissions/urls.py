117a866ec71c77a9b8ce1a4c90ce0db875129042
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-06 08:32:44 -0500

    New 'show submission' page; patch a few bugs and other things

--- tin/apps/submissions/urls.py
+++ tin/apps/submissions/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "submissions"
 
 urlpatterns = [
+    path("<int:submission_id>", views.show_view, name = "show"),
 ]
--- tin/apps/submissions/views.py
+++ tin/apps/submissions/views.py
@@ -1,3 +1,43 @@
+from django import http
 from django.shortcuts import render
 
+from .models import Submission
+
 # Create your views here.
+def show_view(request, submission_id):
+    try:
+        submission = Submission.objects.get(id = submission_id)
+    except Assignment.DoesNotExist:
+        raise http.Http404
+
+    student_submissions = Submission.objects.filter(student = submission.student, assignment = submission.assignment).order_by("date_submitted")
+    submission_number = list(student_submissions).index(submission) + 1
+    if request.user.is_student:
+        if submission.assignment.course in request.user.courses.all():
+            return render(
+                request,
+                "submissions/show.html",
+                {
+                    "course": submission.assignment.course,
+                    "assignment": submission.assignment,
+                    "submission": submission,
+                    "submission_number": submission_number,
+                },
+            )
+        else:
+            raise http.Http404
+    else:
+        if request.user == submission.assignment.course.teacher:
+            return render(
+                request,
+                "submissions/show.html",
+                {
+                    "course": submission.assignment.course,
+                    "assignment": submission.assignment,
+                    "submission": submission,
+                    "student": submission.student,
+                    "submission_number": submission_number,
+                },
+            )
+        else:
+            raise http.Http404
