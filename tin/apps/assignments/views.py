01dc13c62c980c12584111977250ba8782a2fec2
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-05 22:39:38 -0500

    Rework assignment info view logic

--- tin/apps/assignments/views.py
+++ tin/apps/assignments/views.py
@@ -11,40 +11,41 @@ def show_view(request, assignment_id):
     try:
         assignment = Assignment.objects.get(id = assignment_id)
     except Assignment.DoesNotExist:
-        pass
-    else:
-        if request.user.is_student:
-            if assignment.course in request.user.courses.all():
-                submissions = Submission.objects.filter(student = request.user, assignment = assignment).order_by("date_submitted")
-                latest_submission = (submissions.latest("date_submitted") if submissions else None)
-                return render(
-                    request,
-                    "assignments/show.html",
-                    {
-                        "course": assignment.course,
-                        "assignment": assignment,
-                        "submissions": submissions,
-                        "latest_submission": latest_submission,
-                    },
-                )
+        raise http.Http404
+
+    if request.user.is_student:
+        if assignment.course in request.user.courses.all():
+            submissions = Submission.objects.filter(student = request.user, assignment = assignment).order_by("date_submitted")
+            latest_submission = (submissions.latest("date_submitted") if submissions else None)
+            return render(
+                request,
+                "assignments/show.html",
+                {
+                    "course": assignment.course,
+                    "assignment": assignment,
+                    "submissions": submissions,
+                    "latest_submission": latest_submission,
+                },
+            )
         else:
-            if request.user == assignment.course.teacher:
-                students_and_submissions = []
-                for student in assignment.course.students.all():
-                    student_submissions = Submission.objects.filter(student = request.user, assignment = assignment)
-                    if student_submissions:
-                        students_and_submissions.append((student, student_submissions.latest("date_submitted")))
-                    else:
-                        students_and_submissions.append((student, None))
+            raise http.Http404
+    else:
+        if request.user == assignment.course.teacher:
+            students_and_submissions = []
+            for student in assignment.course.students.all():
+                student_submissions = Submission.objects.filter(student = request.user, assignment = assignment)
+                if student_submissions:
+                    students_and_submissions.append((student, student_submissions.latest("date_submitted")))
+                else:
+                    students_and_submissions.append((student, None))
 
-                return render(
-                    request,
-                    "assignments/show.html",
-                    {
-                        "course": assignment.course,
-                        "assignment": assignment,
-                    },
-                )
-
-    raise http.Http404
-
+            return render(
+                request,
+                "assignments/show.html",
+                {
+                    "course": assignment.course,
+                    "assignment": assignment,
+                },
+            )
+        else:
+            raise http.Http404
