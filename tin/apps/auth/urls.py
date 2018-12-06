46a2f76d49a6ee22c8f4b917fb6ec67d8694fbb1
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-06 12:34:12 -0500

    Add password login view

--- tin/apps/auth/urls.py
+++ tin/apps/auth/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as django_auth_views
 
 from . import views
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("", views.index_view, name = "index"),
     path("login/", views.login_view, name = "login"),
     path("logout/", views.logout_view, name = "logout"),
+    path("password-login/", django_auth_views.LoginView.as_view(template_name = "password-login.html"), name = "password_login"),
 ]
