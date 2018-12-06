df6f8a8144aa70568c78c82eb19093f5ddbb1fa8
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-06 17:19:01 -0500

    Only enable password login in debug mode

--- tin/apps/auth/urls.py
+++ tin/apps/auth/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf import settings
 from django.contrib.auth import views as django_auth_views
 
 from . import views
@@ -9,5 +10,8 @@ urlpatterns = [
     path("", views.index_view, name = "index"),
     path("login/", views.login_view, name = "login"),
     path("logout/", views.logout_view, name = "logout"),
-    path("password-login/", django_auth_views.LoginView.as_view(template_name = "password-login.html"), name = "password_login"),
 ]
+if settings.DEBUG:
+    urlpatterns.append(
+        path("password-login/", django_auth_views.LoginView.as_view(template_name = "password-login.html"), name = "password_login")
+    )
