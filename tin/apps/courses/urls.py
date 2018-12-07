f041f5bc3eaf13ca6dff71d1c0fbf292686252e1
Theo Ouzhinski <touzhinski@gmail.com>
2018-12-06 22:21:42 -0500

    Add url and template for roster view

--- tin/apps/courses/urls.py
+++ tin/apps/courses/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", views.index_view, name = "index"),
     path("add/", views.create_view, name = "create"),
     path("<int:course_id>", views.show_view, name = "show"),
-    path("<int:course_id>/edit", views.edit_view, name = "edit")
+    path("<int:course_id>/edit", views.edit_view, name = "edit"),
+    path("<int:course_id>/students", views.students_view, name = "students")
 ]
