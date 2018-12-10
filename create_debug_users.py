05c107a671a12407842a06b6d58cf663944293ac
2020jbeutner <2020jbeutner@tjhsst.edu>
2018-12-10 18:19:19 -0500

    Set is_staff in create_debug_users.py

--- create_debug_users.py
+++ create_debug_users.py
@@ -11,11 +11,11 @@ from tin.apps.users.models import User
 password = input("Enter password for all users: ")
 
 users = [
-    #[username, id, full_name, first_name, last_name, email, is_teacher, is_student, is_superuser]
-    ["2020jbeutner", 33538, "John Beutner", "John", "Beutner", "2020jbeutner@tjhsst.edu", False, True, False],
-    ["2020fouzhins", 33797, "Theo Ouzhinski", "Theo", "Ouzhinski", "2020fouzhins@tjhsst.edu", False, True, False],
-    ["pcgabor", None, "Peter Gabor", "Peter", "Gabor", "pcgabor@fcps.edu", True, False, False],
-    ["admin", None, "Admin", "Admin", "Admin", "admin@example.com", False, False, True],
+    #[username, id, full_name, first_name, last_name, email, is_teacher, is_student, is_staff, is_superuser]
+    ["2020jbeutner", 33538, "John Beutner", "John", "Beutner", "2020jbeutner@tjhsst.edu", False, True, False, False],
+    ["2020fouzhins", 33797, "Theo Ouzhinski", "Theo", "Ouzhinski", "2020fouzhins@tjhsst.edu", False, True, False, False],
+    ["pcgabor", None, "Peter Gabor", "Peter", "Gabor", "pcgabor@fcps.edu", True, False, False, False],
+    ["admin", None, "Admin", "Admin", "Admin", "admin@example.com", False, False, True, True],
 ]
 for user_info in users:
     print("Creating user {}".format(user_info[0]))
@@ -31,7 +31,8 @@ for user_info in users:
     user.email = user_info[5]
     user.is_teacher = user_info[6]
     user.is_student = user_info[7]
-    user.is_superuser = user_info[8]
+    user.is_staff = user_info[8]
+    user.is_superuser = user_info[9]
     user.set_password(password)
     user.save()
 
