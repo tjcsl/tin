85ef9b0c36cc457c45b566d3c95492dd56b948f5
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-04-02 22:57:45 -0400

    Start page of sample graders

--- tin/apps/docs/urls.py
+++ tin/apps/docs/urls.py
@@ -7,5 +7,6 @@ app_name = "docs"
 urlpatterns = [
     path("", views.index_view, name = "index"),
     path("graders", views.graders_view, name = "graders"),
+    path("sample-graders", views.sample_graders_view, name = "sample-graders"),
 ]
 
