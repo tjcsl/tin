5d9161584416acf8cf90b87fc27b7e6eb3fc66b6
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-04-02 22:57:45 -0400

    Start page of sample graders

--- tin/apps/docs/views.py
+++ tin/apps/docs/views.py
@@ -13,6 +13,7 @@ def index_view(request):
             "docs_app": True,
             "pages": {
                 reverse("docs:graders"): "Graders",
+                reverse("docs:sample-graders"): "Sample graders",
             },
         },
     )
@@ -20,4 +21,8 @@ def index_view(request):
 @teacher_or_superuser_required
 def graders_view(request):
     return render(request, "docs/graders.html", {"docs_app": True, "nav_item": "Graders"})
+
+@teacher_or_superuser_required
+def sample_graders_view(request):
+    return render(request, "docs/sample_graders.html", {"docs_app": True, "nav_item": "Sample graders"})
 
