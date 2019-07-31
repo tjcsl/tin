7ee447380c2e9e6135008b0b09e7ce7e8064b2d4
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-07-31 00:09:43 -0400

    Fix celery config loading

--- tin/celery.py
+++ tin/celery.py
@@ -13,8 +13,7 @@ app = Celery('tin')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-#app.config_from_object('django.conf:settings', namespace='CELERY')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
