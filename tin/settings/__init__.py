71d9d173785b6aecd5b1b1f70dd851c4f6e6abac
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-09-03 18:19:09 -0400

    Add Sentry support

--- tin/settings/__init__.py
+++ tin/settings/__init__.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -210,6 +213,11 @@ LOGGING = {
 CELERY_RESULT_BACKEND = "django-db"
 
 
+# Sentry settings
+
+SENTRY_PUBLIC_DSN = None
+
+
 # Tin-specific settings
 
 SUBMISSION_SIZE_LIMIT = 1 * 1000 * 1000  # 1 MB
@@ -221,3 +229,10 @@ try:
     from .secret import *  # noqa
 except ImportError:
     pass
+
+
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=SENTRY_PUBLIC_DSN,
+        integrations=[DjangoIntegration()]
+    )
