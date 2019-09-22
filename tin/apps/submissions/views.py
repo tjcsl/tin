2dff746727e410d2b19933d85f4bb6c1a95c8df0
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-09-22 00:17:49 -0400

    Allow students and teachers to kill submissions
    It's very limited and it takes up to 15 seconds to run, but it works

--- tin/apps/submissions/views.py
+++ tin/apps/submissions/views.py
@@ -75,6 +75,7 @@ def show_json_view(request, submission_id):
             "grader_errors": submission.grader_errors,
             "has_been_graded": submission.has_been_graded,
             "complete": submission.complete,
+            "kill_requested": submission.kill_requested,
             "points_received": (
                 str(submission.points_received) if submission.points_received is not None else None
             ),
@@ -95,6 +96,28 @@ def show_json_view(request, submission_id):
         return http.JsonResponse(data)
 
     return http.Http404
+
+
+@login_required
+def kill_view(request, submission_id):
+    try:
+        submission = Submission.objects.get(id=submission_id)
+    except Submission.DoesNotExist:
+        raise http.Http404
+
+    if request.method == "POST" and (
+        (request.user.is_student and submission.assignment.course in request.user.courses.all())
+        or request.user == submission.assignment.course.teacher
+        or request.user.is_superuser
+    ):
+        submission.kill_requested = True
+        submission.save()
+        if request.GET.get("next"):
+            return redirect(request.GET["next"])
+        else:
+            return redirect("submissions:show", submission.id)
+    else:
+        raise http.Http404
 
 
 @superuser_required
