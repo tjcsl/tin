5a43a49badda18e51b580cb3a8a7d08f21cd8ae3
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-10-03 22:28:24 -0400

    Add drag-and-drop submission feature

---
+++ tin/static/js/upload-dragdrop.js
@@ -0,0 +1,42 @@
+$(function() {
+  if(assignment_submit_url) {
+    var upload_overlay = $("<div>").css({"display": "none", "position": "fixed", "top": 0, "left": 0, "width": "100vw", "height": "100vh", "background-color": "rgba(0, 0, 0, 0.2)", "z-index": 1000, "text-align": "center", "border": "10px dashed #000", "padding-top": 100, "font-size": "250%", "box-sizing": "border-box"}).text("Drop to submit").appendTo($("body"));
+
+    $(window).on({
+      "dragover": function(e) {
+        e.preventDefault();
+      },
+      "dragenter": function(e) {
+        var dt = e.originalEvent.dataTransfer;
+        if(dt.types.includes("Files")) {
+          upload_overlay.stop().fadeIn(150);
+        }
+      },
+      "drop": function(e) {
+        var dt = e.originalEvent.dataTransfer;
+        e.preventDefault();
+        if(dt && dt.files.length) {
+          console.log(dt.files);
+          if(dt.files.length != 1) {
+            alert("Please only upload one file at a time.");
+            return;
+          }
+
+          var form = $("<form>", {"action": assignment_submit_url, "method": "post", "enctype": "multipart/form-data"});
+          $("<input>", {"type": "hidden", "name": "csrfmiddlewaretoken", "value": Cookies.get("csrftoken")}).appendTo(form);
+
+          $("<input>", {"type": "file", "name": "file"}).appendTo(form).get(0).files = dt.files;
+
+          form.css({"display": "none"});
+          form.appendTo($("body"));
+
+          form.submit();
+        }
+      },
+    });
+
+    upload_overlay.on("dragleave", function() {
+      upload_overlay.stop().fadeOut(150);
+    });
+  }
+});
