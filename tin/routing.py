cff121e23e1c8a9de782289a7071749a0f5c5dad
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-11-23 12:55:25 -0500

    Close all incoming websocket connections that don't match another route

--- tin/routing.py
+++ tin/routing.py
@@ -1,9 +1,25 @@
+from typing import Optional
+
 from channels.auth import AuthMiddlewareStack
+from channels.generic.websocket import WebsocketConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.urls import path
 
 from .apps.submissions.consumers import SubmissionJsonConsumer
+
+
+class WebsocketCloseConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.close()
+
+    def receive(self, text_data: Optional[str] = None, bytes_data: Optional[bytes] = None):
+        pass
+
+    def disconnect(self, code):
+        pass
+
 
 application = ProtocolTypeRouter(
     {
@@ -11,6 +27,7 @@ application = ProtocolTypeRouter(
             URLRouter(
                 [
                     path("submissions/<int:submission_id>.json", SubmissionJsonConsumer),
+                    path("<path:path>", WebsocketCloseConsumer),
                 ]
             )
         )
