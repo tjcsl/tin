2d08db35b47e3d969b181b8ce69e60d8c7cffe74
2020jbeutner <2020jbeutner@tjhsst.edu>
2019-11-23 21:09:58 -0500

    Formatting

--- tin/apps/submissions/utils.py
+++ tin/apps/submissions/utils.py
@@ -8,14 +8,10 @@ def serialize_submission_info(submission, user) -> Dict[str, Union[None, int, fl
         "complete": submission.complete,
         "kill_requested": submission.kill_requested,
         "points_received": (
-            float(submission.points_received)
-            if submission.points_received is not None
-            else None
+            float(submission.points_received) if submission.points_received is not None else None
         ),
         "points_possible": (
-            float(submission.points_possible)
-            if submission.points_possible is not None
-            else None
+            float(submission.points_possible) if submission.points_possible is not None else None
         ),
         "grade_percent": submission.grade_percent,
         "formatted_grade": submission.formatted_grade,
