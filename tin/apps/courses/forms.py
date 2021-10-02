f431ecaecf2225ac411514b336c975c2d6d7a1df
lauren <pmd7211@gmail.com>
2021-10-02 19:10:05 -0400

    feat: allow period information

--- tin/apps/courses/forms.py
+++ tin/apps/courses/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from ..users.forms import UserMultipleChoiceField
 from ..users.models import User
-from .models import Course
+from .models import Course, Period
 
 
 class CourseForm(forms.ModelForm):
@@ -15,4 +15,16 @@ class CourseForm(forms.ModelForm):
 
     class Meta:
         model = Course
+        fields = ["name", "students"]
+
+class PeriodForm(forms.ModelForm):
+    def __init__(self, course, *args, **kwargs):
+        super(PeriodForm, self).__init__(*args, **kwargs)
+        self.fields["students"] = UserMultipleChoiceField(
+            queryset=course.students.order_by("last_name", "first_name"),
+            required=False,
+        )
+
+    class Meta:
+        model = Period
         fields = ["name", "students"]
--- tin/apps/courses/urls.py
+++ tin/apps/courses/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("<int:course_id>/edit", views.edit_view, name="edit"),
     path("<int:course_id>/students", views.students_view, name="students"),
     path("<int:course_id>/import", views.import_students_view, name="import"),
+    path("<int:course_id>/add_period", views.add_period_view, name="add_period"),
+    path("<int:course_id>/edit_period", views.edit_period_view, name="edit_period"),
 ]
