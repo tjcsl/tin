c07313f6b63496bb3f402cb0ad1c1920f1141ad6
lauren <pmd7211@gmail.com>
2021-10-07 21:46:02 -0400

    feat: allow editing periods

--- tin/apps/courses/admin.py
+++ tin/apps/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Period
 
 
 class CourseAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Period)
