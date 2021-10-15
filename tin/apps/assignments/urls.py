e096f0d8c2dcc3d1e170a4e5d10abb674d7666d3
lauren <pmd7211@gmail.com>
2021-10-15 14:27:42 -0400

    feat: allow assignments to be deleted

--- tin/apps/assignments/urls.py
+++ tin/apps/assignments/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     ),
     path("add/course/<int:course_id>", views.create_view, name="add"),
     path("<int:assignment_id>/edit", views.edit_view, name="edit"),
+    path("<int:assignment_id>/delete", views.delete_view, name="delete"),
     path("<int:assignment_id>/grader/upload", views.upload_grader_view, name="upload_grader"),
     path("<int:assignment_id>/submit", views.submit_view, name="submit"),
     path("<int:assignment_id>/scores_csv", views.scores_csv_view, name="scores_csv"),
