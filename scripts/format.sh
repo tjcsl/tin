0e2537931c198b6777f03d3080b96fc93baabe2f
lauren <pmd7211@gmail.com>
2022-08-02 19:11:44 -0400

    formatting fixes part 1

--- scripts/format.sh
+++ scripts/format.sh
@@ -10,4 +10,4 @@ done
 
 # Order is important. There are a few things that black and autopep8 disagree on, and I side
 # with autopep8 on those.
-black tin && autopep8 --in-place --recursive tin && isort --recursive tin
+black tin && autopep8 --in-place --recursive tin && isort tin
