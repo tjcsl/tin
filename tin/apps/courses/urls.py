2ec5e0ca8981e37b1e7f1b1bc2b6610b31db5ef2
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-03 23:43:43 -0400

    Clearly differentiate assignment import from student import, fix collision bug

--- tin/apps/courses/urls.py
+++ tin/apps/courses/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     path("<int:course_id>", views.show_view, name="show"),
     path("add/", views.create_view, name="create"),
     path("<int:course_id>/edit", views.edit_view, name="edit"),
-    path("<int:course_id>/import", views.import_select_course_view, name="import"),
+    path("<int:course_id>/import", views.import_select_course_view, name="import_select_course"),
     path(
         "<int:course_id>/import/<int:other_course_id>",
         views.import_from_selected_course,
         name="import_from_selected_course",
     ),
     path("<int:course_id>/students", views.students_view, name="students"),
-    path("<int:course_id>/import", views.import_students_view, name="import"),
-    path("<int:course_id>/manage", views.manage_students_view, name="manage_students"),
+    path("<int:course_id>/students/import", views.import_students_view, name="import_students"),
+    path("<int:course_id>/students/manage", views.manage_students_view, name="manage_students"),
     path("<int:course_id>/add_period", views.add_period_view, name="add_period"),
     path("<int:course_id>/edit_period/<int:period_id>", views.edit_period_view, name="edit_period"),
 ]
