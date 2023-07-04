f83475d5d5067fa9b4e7e55b7bca614c856e7a98
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-03 23:08:15 -0400

    Customize Django admin for assignments

--- tin/apps/assignments/admin.py
+++ tin/apps/assignments/admin.py
@@ -1,12 +1,79 @@
 from django.contrib import admin
+from django.db.models import ManyToManyField
 
-from .models import Assignment, LogMessage, Quiz
+from .models import Assignment, LogMessage, Quiz, Folder
 
 
+@admin.register(Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = "due"
+    list_display = ("name", "course_name", "folder", "due", "visible", "quiz_icon")
+    list_filter = ("language", "course", "due")
+    ordering = ("-due",)
+    save_as = True
+    search_fields = ("name",)
+
+    @admin.display(description="Course")
+    def course_name(self, obj):
+        return obj.course.name
+
+    @admin.display(description="Visible", boolean=True)
+    def visible(self, obj):
+        return not obj.hidden
+
+    @admin.display(description="Quiz", boolean=True)
+    def quiz_icon(self, obj):
+        return bool(obj.is_quiz)
 
 
-admin.site.register(Assignment, AssignmentAdmin)
-admin.site.register(LogMessage)
-admin.site.register(Quiz)
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    date_hierarchy = "assignment__due"
+    list_display = ("assignment", "course_name", "folder_name", "due", "action", "visible")
+    list_filter = ("action",)
+    ordering = ("-assignment__due",)
+    save_as = True
+    search_fields = ("assignment__name",)
+
+    @admin.display(description="Due")
+    def due(self, obj):
+        return obj.assignment.due
+
+    @admin.display(description="Course")
+    def course_name(self, obj):
+        return obj.assignment.course.name
+
+    @admin.display(description="Folder")
+    def folder_name(self, obj):
+        return obj.assignment.folder.name if obj.assignment.folder else None
+
+    @admin.display(description="Visible", boolean=True)
+    def visible(self, obj):
+        return not obj.assignment.hidden
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ("name", "course", "assignments")
+    list_filter = ("course",)
+    ordering = ("course", "name")
+    save_as = True
+    search_fields = ("name",)
+
+    @admin.display(description="Assignments")
+    def assignments(self, obj):
+        return len(obj.assignments.all())
+
+
+@admin.register(LogMessage)
+class LogMessageAdmin(admin.ModelAdmin):
+    date_hierarchy = "date"
+    list_display = ("content", "assignment", "student", "date", "severity")
+    list_filter = ("student", "severity")
+    ordering = ("-date",)
+    save_as = True
+    search_fields = ("quiz__assignment__name", "student__username", "content")
+
+    @admin.display(description="Assignment")
+    def assignment(self, obj):
+        return obj.quiz.assignment.name
