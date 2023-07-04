06180a4ff9bd76ebfe89286e1c4ae3c9e778fc8e
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-04 13:34:34 -0400

    Customize Django admin for users

--- tin/apps/users/admin.py
+++ tin/apps/users/admin.py
@@ -5,8 +5,26 @@ from .models import User
 # Register your models here.
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(User, UserAdmin)
+    date_hierarchy = "date_joined"
+    list_display = (
+        "username",
+        "full_name",
+        "email",
+        "is_student",
+        "is_teacher",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = (
+        "date_joined",
+        "last_login",
+        "is_student",
+        "is_teacher",
+        "is_staff",
+        "is_superuser",
+    )
+    ordering = ("username",)
+    save_as = True
+    search_fields = ("username", "full_name")
