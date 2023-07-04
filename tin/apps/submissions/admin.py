114135a783de383d306889f7b9aaff2dacc3b243
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-04 00:10:12 -0400

    Customize Django admin for submissions

--- tin/apps/submissions/admin.py
+++ tin/apps/submissions/admin.py
@@ -5,8 +5,28 @@ from .models import Submission
 # Register your models here.
 
 
+@admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = "date_submitted"
+    list_display = (
+        "id",
+        "assignment_name",
+        "student_name",
+        "date_submitted",
+        "complete",
+        "has_been_graded",
+        "points_received",
+        "kill_requested",
+    )
+    list_filter = ("assignment__course", "assignment__due")
+    ordering = ("-date_submitted",)
+    save_as = True
+    search_fields = ("assignment__name", "student__username")
 
+    @admin.display(description="Assignment")
+    def assignment_name(self, obj):
+        return obj.assignment.name
 
-admin.site.register(Submission, SubmissionAdmin)
+    @admin.display(description="Student")
+    def student_name(self, obj):
+        return obj.student.username
