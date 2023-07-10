436451f8b89d5c22dc28b9556f1577a314851f47
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-09 22:47:49 -0400

    Basic comment view and template

--- tin/apps/submissions/urls.py
+++ tin/apps/submissions/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("<int:submission_id>", views.show_view, name="show"),
     path("<int:submission_id>.json", views.show_json_view, name="show_json"),
     path("<int:submission_id>/kill", views.kill_view, name="kill"),
+    path("<int:submission_id>/comment", views.comment_view, name="comment"),
     path("set-aborted-complete", views.set_aborted_complete_view, name="set_aborted_complete"),
     path(
         "set-past-timeout-complete",
--- tin/apps/submissions/views.py
+++ tin/apps/submissions/views.py
@@ -5,7 +5,7 @@ from django.db.models import F
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
-from ..auth.decorators import login_required, superuser_required
+from ..auth.decorators import login_required, superuser_required, teacher_or_superuser_required
 from .models import Submission
 from .utils import serialize_submission_info
 
@@ -68,6 +68,39 @@ def kill_view(request, submission_id):
             return redirect("submissions:show", submission.id)
     else:
         raise http.Http404
+
+
+@teacher_or_superuser_required
+def comment_view(request, submission_id):
+    submission = get_object_or_404(
+        Submission.objects.filter_editable(request.user), id=submission_id
+    )
+
+    if not submission.complete or not submission.has_been_graded:
+        raise http.Http404
+
+    before_submissions = Submission.objects.filter(
+        student=submission.student, assignment=submission.assignment, id__lt=submission.id
+    )
+    submission_number = before_submissions.count() + 1
+
+    with open(submission.backup_file_path, "r", encoding="utf-8") as f_obj:
+        submission_text = f_obj.read()
+
+    if request.method == "POST":
+        return redirect("submissions:show", submission.id)
+
+    context = {
+        "course": submission.assignment.course,
+        "folder": submission.assignment.folder,
+        "assignment": submission.assignment,
+        "submission": submission,
+        "nav_item": "Comment",
+        "submission_number": submission_number,
+        "submission_text": submission_text,
+    }
+
+    return render(request, "submissions/comment.html", context)
 
 
 @superuser_required
