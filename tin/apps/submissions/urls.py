ef78e8425e3a9ffac43fe7b738bad6f0c4be66af
Krishnan Shankar <krishnans2006@gmail.com>
2023-12-04 14:38:41 -0500

    Add comment editing and deleting

--- tin/apps/submissions/urls.py
+++ tin/apps/submissions/urls.py
@@ -10,6 +10,16 @@ urlpatterns = [
     path("<int:submission_id>/kill", views.kill_view, name="kill"),
     path("<int:submission_id>/rerun", views.rerun_view, name="rerun"),
     path("<int:submission_id>/comment", views.comment_view, name="comment"),
+    path(
+        "<int:submission_id>/comment/edit/<int:comment_id>",
+        views.edit_comment_view,
+        name="edit_comment",
+    ),
+    path(
+        "<int:submission_id>/comment/delete/<int:comment_id>",
+        views.delete_comment_view,
+        name="delete_comment",
+    ),
     path("<int:submission_id>/publish", views.publish_view, name="publish"),
     path("<int:submission_id>/unpublish", views.unpublish_view, name="unpublish"),
     path("filter", views.filter_view, name="filter"),
