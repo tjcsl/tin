a03625c0d1d8de9b87b84e70162631fe8b9df2b5
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-26 11:50:37 -0400

    Clean up assertions

--- tin/tests/assertions.py
+++ tin/tests/assertions.py
@@ -5,6 +5,8 @@ checks throughout the tests
 
 from __future__ import annotations
 
+from typing import Literal
+
 from django.http import HttpResponse, HttpResponseRedirect
 
 __all__ = (
@@ -15,11 +17,37 @@ __all__ = (
 )
 
 
-def is_redirect(response: HttpResponse) -> bool:
-    return isinstance(response, HttpResponseRedirect) and response.status_code == 302
+def is_redirect(
+    response: HttpResponse, url: str | None = None, part: Literal["base", "full", "end"] = "full"
+) -> bool:
+    """
+    Checks if ``response`` is a redirect.
+
+    Parameters
+    ----------
+        url
+            Checks if redirect url is the same as url
+
+        part
+            Which part of the url to check. Can be base, full, or end.
+    """
+    if not (isinstance(response, HttpResponseRedirect) and response.status_code == 302):
+        return False
+
+    if url is None:
+        return True
+
+    if part == "base":
+        return response.url.startswith(url)
+    if part == "full":
+        return response.url == url
+    return part.endswith(url)
 
 
 def not_redirect(response: HttpResponse) -> bool:
+    """
+    Inverse of :meth:`is_redirect`
+    """
     return not is_redirect(response)
 
 
@@ -33,14 +61,7 @@ def is_login_redirect(response: HttpResponse, next: str | None = None) -> bool:
     if next is not None and isinstance(next, str):
         login_success_url += f"?next={next}"
 
-    return (
-        # typecheckers don't realize is_redirect already makes sure
-        # it is an HttpResponseRedirect, so we have to duplicate
-        # that function here
-        isinstance(response, HttpResponseRedirect)
-        and response.status_code == 302
-        and response.url.startswith(login_success_url)
-    )
+    return is_redirect(response, url=login_success_url, part="base")
 
 
 def not_login_redirect(response: HttpResponse, **kwargs: str | None) -> bool:
