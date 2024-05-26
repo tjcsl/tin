ea4c3d4bf75369616ba1623cc4c2908da13ed179
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-26 09:45:55 -0400

    Stronger typing on utils

--- tin/tests/utils.py
+++ tin/tests/utils.py
@@ -2,21 +2,37 @@ from __future__ import annotations
 
 __all__ = ("admin", "teacher", "student")
 
-from typing import Callable
+from typing import TYPE_CHECKING, Any, Callable, overload
 
 import pytest
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec, TypeVar
+
+    T = TypeVar("T", bound=object, default=None)
+    P = ParamSpec("P")
+
+    TestFunction = Callable[..., object]
 
 
 # we need this function to take into account usage as
 # @admin() or @admin
-def apply_fixture(__f: Callable[..., object] | None, /, prefix: str, **kwargs):
-    fixture = pytest.mark.usefixtures(f"{prefix}_login", **kwargs)
+@overload
+def apply_fixture(__f: Callable[P, T], prefix: str, **kwargs: Any) -> Callable[P, T]: ...
+
+
+@overload
+def apply_fixture(__f: None, prefix: str, **kwargs: Any) -> TestFunction: ...
+
+
+def apply_fixture(__f: TestFunction | None, /, prefix: str, **kwargs: Any):
+    fixture = pytest.mark.usefixtures(prefix, **kwargs)
     if __f is not None and callable(__f):
         return fixture(__f)
     return fixture
 
 
-def admin(__f: Callable[..., object] | None = None, /, **kwargs):
+def admin(__f: TestFunction | None = None, /, **kwargs: Any) -> TestFunction:
     """
     Log in as an admin.
 
@@ -27,14 +43,14 @@ def admin(__f: Callable[..., object] | None = None, /, **kwargs):
             # client is logged in as an admin
 
 
-        @admin(kwargs_for_usefixtures=xyz)
+        @admin(kwarg_for_usefixtures=xyz)
         def test_something(client):
             # client is logged in as an admin
     """
-    return apply_fixture(__f, "admin", **kwargs)
+    return apply_fixture(__f, "admin_login", **kwargs)
 
 
-def teacher(__f: Callable[..., object] | None = None, /, **kwargs):
+def teacher(__f: TestFunction | None = None, /, **kwargs: Any) -> TestFunction:
     """
     Log in as a teacher
 
@@ -45,14 +61,14 @@ def teacher(__f: Callable[..., object] | None = None, /, **kwargs):
             # client is logged in as a teacher
 
 
-        @teacher(kwargs_for_usefixtures=xyz)
+        @teacher(kwarg_for_usefixtures=xyz)
         def test_something(client):
             # client is logged in as a teacher
     """
-    return apply_fixture(__f, "teacher", **kwargs)
+    return apply_fixture(__f, "teacher_login", **kwargs)
 
 
-def student(__f: Callable[..., object] | None = None, /, **kwargs):
+def student(__f: TestFunction | None = None, /, **kwargs: Any) -> TestFunction:
     """
     Log in as a student
 
@@ -63,8 +79,8 @@ def student(__f: Callable[..., object] | None = None, /, **kwargs):
             # client is logged in as a student
 
 
-        @student(kwargs_for_usefixtures=xyz)
+        @student(kwarg_for_usefixtures=xyz)
         def test_something(client):
             # client is logged in as a student
     """
-    return apply_fixture(__f, "student", **kwargs)
+    return apply_fixture(__f, "student_login", **kwargs)
