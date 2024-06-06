f39ae66a5447369b640ebda84cdb9b2208a7496f
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-06-05 23:08:53 -0400

    Improve testing CI+utils infrastructure
    Also uses pytest-xdist to run tests in parallel

--- tin/tests/utils.py
+++ tin/tests/utils.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
-__all__ = ("login",)
+__all__ = (
+    "login",
+    "str_to_html",
+    "to_html",
+)
 
-from typing import TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING, Callable
 
 import pytest
+from django.template import Context, Engine
 
 if TYPE_CHECKING:
     from typing_extensions import ParamSpec, TypeVar
@@ -13,7 +18,7 @@ if TYPE_CHECKING:
     P = ParamSpec("P")
 
 
-def login(user: str, *args: Any) -> Callable[[Callable[P, T]], Callable[P, T]]:
+def login(user: str) -> Callable[[Callable[P, T]], Callable[P, T]]:
     """Login ``client`` as a tin user type.
 
     .. code-block::
@@ -35,4 +40,39 @@ def login(user: str, *args: Any) -> Callable[[Callable[P, T]], Callable[P, T]]:
             response = client.post(reverse("courses:index"), {})
             assert is_login_redirect(response)
     """
-    return pytest.mark.usefixtures(f"{user}_login", *args)
+    return pytest.mark.usefixtures(f"{user}_login")
+
+
+# we define these helper functions to avoid hardcoding
+# stuff like the html escape code for '
+def to_html(s: str, ctx: dict[str, str]):
+    """Convert template code to an html string
+
+    .. code-block:: pycon
+
+        >>> template_logic = "Hello, my name is {{ username }}!"
+        >>> context = {"username": "2027adeshpan"}
+        >>> to_html(template_logic, context)
+        'Hello, my name is 2027adeshpan!'
+
+    Args:
+        s: The template string (see :class:`~django.template.Template`)
+        ctx: The variables/context to use for ``s`` (see :class:`~django.template.Context`)
+    """
+    template = Engine().from_string(s)
+    context = Context(ctx)
+    return template.render(context)
+
+
+def str_to_html(s: str):
+    """Converts a string to it's html representation
+
+    .. code-block:: pycon
+
+        >>> text = "It's annoying to remember HTML escape codes"
+        >>> str_to_html(text)
+        'It&#x27;s annoying to remember HTML escape codes'
+    """
+    template = "{{ var }}"
+    ctx = {"var": s}
+    return to_html(template, ctx)
