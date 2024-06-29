913a642019f7bd70b7aae81047869f134c898f91
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-06-29 15:55:27 -0400

    some more basic tests + typehints

--- tin/apps/submissions/tests.py
+++ tin/apps/submissions/tests.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 from django.urls import reverse
 
-from tin.tests import login
+from tin.tests import is_redirect, login
+
+if TYPE_CHECKING:
+    from django.test import Client
+
+    from ..courses.models import Course
+    from .models import Submission
 
 
 @login("student")
@@ -21,7 +29,7 @@ from tin.tests import login
     ),
 )
 def test_see_submission_after_archived(
-    client, course, submission, perm: str, hidden: bool, archived: bool
+    client: Client, course: Course, submission: Submission, perm: str, hidden: bool, archived: bool
 ):
     course.permission = perm
     course.archived = archived
@@ -29,3 +37,19 @@ def test_see_submission_after_archived(
 
     response = client.get(reverse("submissions:show", args=[submission.id]))
     assert (response.status_code == 404) is hidden
+
+
+@login("student")
+def test_student_requests_kill(client: Client, submission: Submission):
+    response = client.post(reverse("submissions:kill", args=[submission.id]))
+    submission.refresh_from_db()
+    assert is_redirect(response)
+    assert submission.kill_requested
+
+
+@login("teacher")
+def test_teacher_requests_kill(client: Client, submission: Submission):
+    response = client.post(reverse("submissions:kill", args=[submission.id]))
+    submission.refresh_from_db()
+    assert is_redirect(response)
+    assert submission.kill_requested
