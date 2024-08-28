66ccc3b684ae5576c411e960525010f7a30a2874
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-08-28 15:53:21 -0400

    add tests for Submissions app

---
+++ tin/apps/submissions/tests/test_models.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from ..models import Submission, upload_submission_file_path
+
+
+def test_submission_save_file(settings, submission: Submission):
+    file_path = upload_submission_file_path(submission, "")
+    submission.save_file("something")
+    assert submission.file.name == file_path
+
+    submission_path = submission.file_path
+    assert submission_path is not None
+    submission_path = Path(submission_path)
+    assert submission_path == Path(settings.MEDIA_ROOT) / file_path
+    assert submission_path.exists()
+
+
+def test_make_submission_backup(submission: Submission):
+    submission.create_backup_copy("HI")
+    backup_file = submission.backup_file_path
+    assert backup_file is not None
+    backup_path = Path(backup_file)
+    assert backup_path.exists()
+    assert backup_path.read_text("utf-8") == "HI"
