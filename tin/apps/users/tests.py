7f8b6a3de4ca5ec0a09cc2a4f953a4ffeee09671
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-11-10 10:49:21 -0500

    Ensure that test doesn't autocreate users before

--- tin/apps/users/tests.py
+++ tin/apps/users/tests.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
+import pytest
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 
 
+@pytest.mark.no_autocreate_users
 def test_create_debug_users():
+    admin = get_user_model().objects.filter(username="admin", is_superuser=True)
+    student = get_user_model().objects.filter(username="student", is_student=True)
+    teacher = get_user_model().objects.filter(username="teacher", is_teacher=True)
+
+    assert not admin.exists()
+    assert not teacher.exists()
+    assert not student.exists()
+
     call_command("create_debug_users", noinput=True, verbosity=0)
-    assert get_user_model().objects.filter(username="admin", is_superuser=True)
-    assert get_user_model().objects.filter(username="student", is_student=True)
-    assert get_user_model().objects.filter(username="teacher", is_teacher=True)
+
+    assert admin.exists()
+    assert teacher.exists()
+    assert student.exists()
