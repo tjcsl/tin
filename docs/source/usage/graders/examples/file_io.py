1e856cf8034d8ba069ce2c9f215fd33b46f0cf2b
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-01-09 18:30:11 -0500

    Use tempfile for file io example

--- docs/source/usage/graders/examples/file_io.py
+++ docs/source/usage/graders/examples/file_io.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import subprocess
 import sys
+import tempfile
 from pathlib import Path
 
 submission = sys.argv[1]
@@ -11,41 +12,34 @@ student_submission = Path(sys.argv[2])
 # make sure to use the absolute path
 INPUT_FILE = (Path(__file__).parent / "input.txt").resolve()
 
-# output file is in the same directory as the student submission
-# This way we can avoid multiple submissions trying to write to
-# the same file.
-# Again, making sure to use the absolute path
-OUTPUT_FILE = (student_submission.parent / "output.txt").resolve()
+# note that since multiple submissions may be running at the same time
+# we should make sure to use a filename that's not already in use
+# to prevent different submissions from trying to access the same file.
+with tempfile.NamedTemporaryFile(dir=student_submission.parent) as f:
+    command = [
+        sys.executable,
+        submission,
+        # give read permissions to the input
+        # making sure to use the absolute path to the file
+        "--read",
+        INPUT_FILE,
+        # and allow them to read/write to the output file
+        "--write",
+        f.name,
+        # and then pass the arguments to the student submission
+        "--",
+        INPUT_FILE,
+        f.name,
+    ]
 
+    resp = subprocess.run(
+        command,
+        stdout=sys.stdout,
+        stderr=subprocess.STDOUT,
+        check=False,
+    )
 
-command = [
-    sys.executable,
-    submission,
-    # give read permissions to the input
-    # making sure to use the absolute path to the file
-    "--read",
-    INPUT_FILE,
-    # and allow them to read/write to the output file
-    "--write",
-    OUTPUT_FILE,
-    # and then pass the arguments to the student submission
-    "--",
-    INPUT_FILE,
-    OUTPUT_FILE,
-]
-
-resp = subprocess.run(
-    command,
-    stdout=sys.stdout,
-    stderr=subprocess.STDOUT,
-    check=False,
-)
-
-if (
-    resp.returncode != 0
-    or not OUTPUT_FILE.exists()
-    or OUTPUT_FILE.read_text() != INPUT_FILE.read_text()
-):
-    print("Score: 0%")
-else:
-    print("Score: 100%")
+    if resp.returncode != 0 or Path(f.name).read_text() != INPUT_FILE.read_text():
+        print("Score: 0%")
+    else:
+        print("Score: 100%")
