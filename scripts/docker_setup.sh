d384d54ab05357338ba20f9e012567405e0ef7b2
Krishnan Shankar <krishnans2006@gmail.com>
2026-04-07 13:59:50 -0500

    Fix docker setup script
    This updates the script's compatibility with changes made in 53fad2e7989e417ac3210d61b4ab6f82f0595484

--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -5,7 +5,7 @@ if [ ! "$(docker ps -q -f name=tin_django)" ]; then
     exit 1
 fi
 
-docker exec tin_django /bin/sh -c "cd /app && python manage.py migrate && python manage.py create_debug_users --noinput"
+docker exec tin_django /bin/sh -c "cd /app && uv run python manage.py migrate && uv run python manage.py create_debug_users --noinput"
 echo
 echo "Done! Visit http://localhost:8000 to access the development server."
 echo "The default password for all users is 'jasongrace'."
